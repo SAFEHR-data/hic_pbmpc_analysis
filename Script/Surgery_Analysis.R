af0923c26cc9a0f7c354d73d8172573ad95171ab
muh-arfeen <m.arfeen@ucl.ac.uk>
2025-07-21 11:23:52 +0000

    updated

--- Script/Surgery_Analysis.R
+++ Script/Surgery_Analysis.R
@@ -2,40 +2,50 @@ library(arrow)
 library(tools)
 library(purrr)
 library(tidyverse)
-
+library(omopcept)
 
 read_omop_dataset <- function(path) {
-  batches <- fs::dir_ls(path, type = "directory")
-  
-  # Check if we found any batch folders
-  if (length(batches) == 0) stop("No batch folders found.")
-  
-  # Get one example batch to extract table names
-  first_batch <- batches[1]
-  example_files <- fs::dir_ls(first_batch, glob = "*.parquet")
-  
-  # Check if any files found
-  if (length(example_files) == 0) stop("No .parquet files found in first batch folder.")
-  
-  table_names <- tools::file_path_sans_ext(basename(example_files))
-  
-  omop_ds <- list()
-  
-  for (table in table_names) {
-    files_for_table <- file.path(batches, paste0(table, ".parquet"))
-    existing_files <- files_for_table[file.exists(files_for_table)]
-    omop_ds[[tolower(table)]] <- open_dataset(existing_files)
-  }
-  
-  return(omop_ds)
+  # List all subdirectories (each subdir = a table name like 'visit_occurrence')
+  list.dirs(path, recursive = FALSE) |>
+    # Use folder names as list names
+    set_names(~ basename(.)) |>
+    # For each folder, open it as an Arrow dataset
+    map(arrow::open_dataset)
 }
+# read_omop_dataset <- function(path) {
+#   batches <- fs::dir_ls(path, type = "directory")
+#   
+#   # Check if we found any batch folders
+#   if (length(batches) == 0) stop("No batch folders found.")
+#   
+#   # Get one example batch to extract table names
+#   first_batch <- batches[1]
+#   example_files <- fs::dir_ls(first_batch, glob = "*.parquet")
+#   
+#   # Check if any files found
+#   if (length(example_files) == 0) stop("No .parquet files found in first batch folder.")
+#   
+#   table_names <- tools::file_path_sans_ext(basename(example_files))
+#   
+#   omop_ds <- list()
+#   
+#   for (table in table_names) {
+#     files_for_table <- file.path(batches, paste0(table, ".parquet"))
+#     existing_files <- files_for_table[file.exists(files_for_table)]
+#     omop_ds[[tolower(table)]] <- open_dataset(existing_files)
+#   }
+#   
+#   return(omop_ds)
+# }
+
+
 
 custom_path <- "/data/mqummeru/Extract_hic_PBPMC/"   # e.g., "/data/omop/custom/"
 
 custom_omop_ds <- read_omop_dataset(custom_path)
 
 
-custom_omop_ds$
+custom_omop_ds$procedure_occurrence_links %>% head() %>% collect() %>% view()
 
 # Extract distinct procedure_occurrence_id values where the plugin provenance is "surgical"
 df_surgical_procedure_id<- custom_omop_ds$procedure_occurrence_links %>% 
@@ -60,6 +70,11 @@ df_procedure_occurance <- custom_omop_ds$procedure_occurrence %>%
 
 # Add human-readable concept names to the dataset using OMOP concept mappings  
 df_procedure_occurance <- df_procedure_occurance %>%omop_join_name_all()
+
+df_procedure_occurance %>%
+  summarise(across(everything(), ~ sum(is.na(.)))) %>%
+  pivot_longer(cols = everything(), names_to = "column", values_to = "na_count") %>%
+  arrange(desc(na_count)) %>% view
 
 
 df_visit_detail <- custom_omop_ds$visit_detail %>%
