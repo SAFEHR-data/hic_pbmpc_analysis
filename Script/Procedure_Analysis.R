060b5de3b02a71c6b48e9b06cfce819e3af21723
muh-arfeen <m.arfeen@ucl.ac.uk>
2025-07-24 09:32:18 +0000

    update the script and file name

--- Script/Procedure_Analysis.R
+++ Script/Procedure_Analysis.R
@@ -3,44 +3,11 @@ library(tools)
 library(purrr)
 library(tidyverse)
 library(omopcept)
-
-read_omop_dataset <- function(path) {
-  # List all subdirectories (each subdir = a table name like 'visit_occurrence')
-  list.dirs(path, recursive = FALSE) |>
-    # Use folder names as list names
-    set_names(~ basename(.)) |>
-    # For each folder, open it as an Arrow dataset
-    map(arrow::open_dataset)
-}
-# read_omop_dataset <- function(path) {
-#   batches <- fs::dir_ls(path, type = "directory")
-#   
-#   # Check if we found any batch folders
-#   if (length(batches) == 0) stop("No batch folders found.")
-#   
-#   # Get one example batch to extract table names
-#   first_batch <- batches[1]
-#   example_files <- fs::dir_ls(first_batch, glob = "*.parquet")
-#   
-#   # Check if any files found
-#   if (length(example_files) == 0) stop("No .parquet files found in first batch folder.")
-#   
-#   table_names <- tools::file_path_sans_ext(basename(example_files))
-#   
-#   omop_ds <- list()
-#   
-#   for (table in table_names) {
-#     files_for_table <- file.path(batches, paste0(table, ".parquet"))
-#     existing_files <- files_for_table[file.exists(files_for_table)]
-#     omop_ds[[tolower(table)]] <- open_dataset(existing_files)
-#   }
-#   
-#   return(omop_ds)
-# }
+library(here)
 
 
+source("/data/mqummeru/hic_pbmpc_analysis/Script/open_omop_dataset.R")
 
-custom_path <- "/data/mqummeru/Extract_hic_PBPMC/"   # e.g., "/data/omop/custom/"
 
 custom_omop_ds <- read_omop_dataset(custom_path)
 
@@ -158,7 +125,8 @@ df_procedure_visit_occurrence <-df_procedure_occurrence %>%
 df_procedure_visit_occurrence %>% view()
 
 # Plot top 50 procedure names by count
-plot_procedures <-df_procedure_visit_occurrence %>%
+
+  df_procedure_occurrence %>%
   count(procedure_concept_name, sort = TRUE) %>%
   top_n(50, n) %>%
   ggplot(aes(x = reorder(procedure_concept_name, n), y = n)) +
---
+++ Script/open_omop_dataset.R
@@ -0,0 +1,37 @@
+read_omop_dataset <- function(path) {
+  # List all subdirectories (each subdir = a table name like 'visit_occurrence')
+  list.dirs(path, recursive = FALSE) |>
+    # Use folder names as list names
+    set_names(~ basename(.)) |>
+    # For each folder, open it as an Arrow dataset
+    map(arrow::open_dataset)
+}
+# read_omop_dataset <- function(path) {
+#   batches <- fs::dir_ls(path, type = "directory")
+#   
+#   # Check if we found any batch folders
+#   if (length(batches) == 0) stop("No batch folders found.")
+#   
+#   # Get one example batch to extract table names
+#   first_batch <- batches[1]
+#   example_files <- fs::dir_ls(first_batch, glob = "*.parquet")
+#   
+#   # Check if any files found
+#   if (length(example_files) == 0) stop("No .parquet files found in first batch folder.")
+#   
+#   table_names <- tools::file_path_sans_ext(basename(example_files))
+#   
+#   omop_ds <- list()
+#   
+#   for (table in table_names) {
+#     files_for_table <- file.path(batches, paste0(table, ".parquet"))
+#     existing_files <- files_for_table[file.exists(files_for_table)]
+#     omop_ds[[tolower(table)]] <- open_dataset(existing_files)
+#   }
+#   
+#   return(omop_ds)
+# }
+
+
+
+custom_path <- "/data/mqummeru/Extract_hic_PBPMC/"   # e.g., "/data/omop/custom/"
